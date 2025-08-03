782e5fb5272676dab0fa74836aea17d83417f903
Amber Forrest <amber4est@gmail.com>
2025-08-03 02:19:56 -0400

    Part 1 and Part 2 of the assignment added (creating objects and working with JSON)

---
+++ script.js
@@ -0,0 +1,30 @@
+partSeperation="--------------------------------------";
+//Part 1: Understanding and Creating Objects
+student = {
+    name: "Amber",
+    age: 22,
+    enrolled: true,
+    courses: ["Writing", "JavaScript", "Web Programming", "Databases","Algorithms", "Operating Systems"],
+
+    displayInformation() {
+        return `\nStudent: ${this.name}\nAge: ${this.age}\nEnrolled: ${this.enrolled}\nCourses: ${this.courses}\n`
+    }
+}
+
+console.log(partSeperation);
+console.log(`Basic information about a student:\nStudent Name: ${student.name}`);
+console.log(`Student Age: ${student.age}\n`);
+console.log("Student's complete information:", student.displayInformation());
+
+//Part 2: Working with JSON
+console.log(partSeperation);
+
+const studentJSON = JSON.stringify(student);
+console.log("Student's information as a JSON string:\n", studentJSON);
+
+const studentFromJSON = JSON.parse(studentJSON);
+console.log("\nStudent's information back in an object:\n", studentFromJSON);
+
+console.log("\nOriginal Student Information:\n", student);
+
+console.log("\nThe student's information in an object from the JSON is missing the method that is in the original student object!");
